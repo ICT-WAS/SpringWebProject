ead58ab29cfe380fba6874ecc38116ab89d8f510
Puim-Hyeonji-Lee <79463979+Lee-Hyeonji99@users.noreply.github.com>
2024-12-03 19:47:30 +0900

    [리액트] 청약 공고 페이지 생성 및 라우터 연동 (#12)
    * [리액트] 조건 페이지 작업 중
    
    * [리액트] 라우터 적용(헤더 버튼으로 메인페이지, 청약 공고 페이지 연결)

--- react-home/home/src/index.js
+++ react-home/home/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Main from "./main/main";
-import "./common/common.css";
+import "./common/Common.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; //라우터 설정
 import AppProvider from "./Context"; //전역변수 설정
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login";
+import ApplyAnnouncement from "./apply_announcement/ApplyAnouncement";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -26,6 +27,7 @@ root.render(
         />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/list" element={<ApplyAnnouncement />} />
       </Routes>
     </Router>
   </AppProvider>
