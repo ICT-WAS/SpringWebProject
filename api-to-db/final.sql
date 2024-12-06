18794ca73d1a9212220a9b15f0e572c6e32afb99
mmparksb <mmparksb@gmail.com>
2024-12-06 22:09:20 +0900

    no message

--- api-to-db/final.sql
+++ api-to-db/final.sql
@@ -36,7 +36,7 @@ CREATE TABLE `DETAIL04` (
 	`GNRL_RCEPT_BGNDE`	DATE	NULL,
 	`GNRL_RCEPT_ENDDE`	DATE	NULL,
 	PRIMARY KEY (`detail04_id`),
-	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)
+	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)    ON DELETE CASCADE
 );
 
 CREATE TABLE `DETAIL01` (
@@ -74,7 +74,7 @@ CREATE TABLE `DETAIL01` (
 	`NPLN_PRVOPR_PUBLIC_HOUSE_AT`	CHAR(1)	NOT NULL	COMMENT 'Y / N',
 	`PUBLIC_HOUSE_SPCLW_APPLC_AT`	CHAR(1)	NOT NULL	COMMENT 'Y / N',
 	PRIMARY KEY (`detail01_id`),
-	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)
+	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)    ON DELETE CASCADE
 );
 
 CREATE TABLE `DETAIL` (
@@ -96,7 +96,7 @@ CREATE TABLE `DETAIL` (
 	`NWBB_HSHLDCO`	INTEGER	NULL	COMMENT '01 전용',
 	`LTTOT_TOP_AMOUNT`	INTEGER	NOT NULL,
 	PRIMARY KEY (`detail_id`),
-	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)
+	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)    ON DELETE CASCADE
 );
 
 CREATE TABLE `member` (
@@ -123,8 +123,9 @@ CREATE TABLE `post` (
 	`title`	VARCHAR(100)	NOT NULL,
 	`subject`	TEXT	NOT NULL,
 	`created_at`	DATETIME	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+	`updated_at`	DATETIME	NULL,
 	PRIMARY KEY (`post_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`)
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `comment` (
@@ -135,10 +136,11 @@ CREATE TABLE `comment` (
 	`comment_id2`	BIGINT	NULL,
 	`comment`	TEXT	NOT NULL,
 	`created_at`	DATETIME	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
+	`updated_at`	DATETIME	NULL,
 	PRIMARY KEY (`comment_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
-	FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
-	FOREIGN KEY (`comment_id2`) REFERENCES `comment` (`comment_id`)
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
+	FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`)   ON DELETE CASCADE,
+	FOREIGN KEY (`comment_id2`) REFERENCES `comment` (`comment_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `wish` (
@@ -149,8 +151,8 @@ CREATE TABLE `wish` (
 	`type`	VARCHAR(30)	NULL	COMMENT 'detail 테이블의 HOUSE_TY 참조',
 	`alarm`	INTEGER	NOT NULL	DEFAULT 1	COMMENT 'default 1 : 알리받기 / 0 : 알림 안받기',
 	PRIMARY KEY (`wish_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
-	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
+	FOREIGN KEY (`house_id`) REFERENCES `house` (`house_id`)    ON DELETE CASCADE,
 	CONSTRAINT `chk_alarm` CHECK (`alarm` IN (0, 1))
 );
 
@@ -164,7 +166,7 @@ CREATE TABLE `account` (
 	`recognized_amount`	INTEGER	NULL	DEFAULT 0	COMMENT '(단위 : 만원)',
 	`relationship`	INTEGER	NOT NULL	COMMENT '1: 본인, 2: 배우자',
 	PRIMARY KEY (`account_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
 	CONSTRAINT `chk_type` CHECK (`type` IN ('청약예금', '청약부금', '청약저축', '주택청약종합저축')),
     CONSTRAINT `chk_relationship` CHECK (`relationship` IN (1, 2))
 );
@@ -180,7 +182,7 @@ CREATE TABLE `family` (
 	`house_count`	INTEGER	NULL    DEFAULT 0,
 	`house_sold_date`	DATE	NULL	COMMENT '날짜 / null 이면 없음',
 	PRIMARY KEY (`family_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
 	CONSTRAINT `chk_living_together` CHECK (`living_together` IN (0, 1, 2)),
     CONSTRAINT `chk_living_together_date` CHECK (`living_together_date` IN (0, 1, 2)),
     CONSTRAINT `chk_is_married` CHECK (`is_married` IN (0, 1))
@@ -193,13 +195,13 @@ CREATE TABLE `condition01` (
 	`si_do`	VARCHAR(60)	NOT NULL,
 	`gun_gu`	VARCHAR(60)	NOT NULL,
 	`transfer_date`	DATE	NOT NULL,
-	`Gyunggi_date`	DATE	NULL,
+	`Gyungi_date`	DATE	NULL,
 	`metropolitan_area_date`	DATE	NULL,
 	`householder`	INTEGER	NOT NULL	COMMENT '0: 본인이 세대원 / 1 : 본인이 세대주',
 	`married`	INTEGER	NOT NULL	COMMENT '0: 미혼 / 1: 기혼 / 2: 예비신혼부부 / 3: 한부모',
 	`married_date`	DATE	NULL,
 	PRIMARY KEY (`condition01_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
     CONSTRAINT `chk_householder` CHECK (`householder` IN (0, 1)),
     CONSTRAINT `chk_married` CHECK (`married` IN (0, 1, 2, 3))
 );
@@ -214,7 +216,7 @@ CREATE TABLE `condition02` (
 	`spouse`	INTEGER	NULL	COMMENT '0 : x / 1 : o',
 	`in_laws`	INTEGER	NULL	COMMENT '0 : x / 1 : o',
 	PRIMARY KEY (`condition02_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
 	CONSTRAINT `chk_grandparents` CHECK (`grandparents` IN (0, 1)),
 	CONSTRAINT `chk_parents` CHECK (`parents` IN (0, 1)),
 	CONSTRAINT `chk_child` CHECK (`child` IN (0, 1)),
@@ -241,7 +243,7 @@ CREATE TABLE `condition03` (
 	`last_winned`	DATE	NULL	COMMENT '가장 최근에 당첨된 날짜 / 없으면 null',
 	`ineligible`	DATE	NULL	COMMENT '부적격자 판정된 날짜 / 없으면 null',
 	PRIMARY KEY (`condition03_id`),
-	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`),
+	FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE,
 	CONSTRAINT `chk_property_price` CHECK (`property_price` IN (0, 1, 2)),
 	CONSTRAINT `chk_is_owner` CHECK (`is_owner` IN (0, 1)),
 	CONSTRAINT `chk_sold_house` CHECK (`sold_house` IN (0, 1))
