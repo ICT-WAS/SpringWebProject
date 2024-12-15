527c5607ae56f6cf4178d136c75e18cc9495266b
Puim-Hyeonji-Lee <79463979+Lee-Hyeonji99@users.noreply.github.com>
2024-12-16 00:42:56 +0900

    [리액트] 조건 등록 기능 구현 (#51)
    * [리액트] 누락파일 업로드 및 조건등록 페이지 유효성 검사 추가
    -유효성 체크를 위해 1페이지 거주지역 드롭다운 -> 셀렉트로 변경
    -조건등록 2페이지 제오외 유효성 검사 추가, 문항 번호 삭제
    
    * [리액트] 조건검색 필터 변경시 새로 검색된 건수 표시, 필터 초기화 기능, 페이지네이션 버그 수정 및 여러 warning 제거
    - 페이지네이션 오류 수정
    - 페이지네이션 검색결과없는경우 >, >> 버튼 못눌리게
    - 페이지네이션 <, > 은 다음 페이지네이션의 첫번재 항목으로 설정
    - 필터 초기화버튼
    - 필터 값 변경시 실시간 건수 표시
    - 필터 로딩상태 : 스피너 추가
    
    * [리액트] 조건등록 테스트 완료
    - 조건등록 2페이지 유효성검증 추가
    - 조건등록 데이터 전송(조건등록)
    - 코드 리팩토링
    
    * [리액트] 조건 등록 기능 구현
    조건등록2 페이지 세대원 삭제 기능 추가
    Account, Condition01, Condition03, Family 데이터 추가된 것 확인

--- home/src/main/java/com/ict/home/condition/dto/AccountDTO.java
+++ home/src/main/java/com/ict/home/condition/dto/AccountDTO.java
@@ -1,61 +1,40 @@
-package com.ict.home.condition.model;
-
+package com.ict.home.condition.dto;
 
 import com.ict.home.condition.enumeration.AccountType;
-import com.ict.home.user.User;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Null;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "account")
-@Getter
-@Setter
-@ToString
-@Schema(title = "청약통장")
-@AllArgsConstructor
-@NoArgsConstructor
-public class Account {
+@Getter @Setter @ToString
+public class AccountDTO {
 
     @Schema(description = "고유 pk")
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "account_id")
     private Long accountId;
 
     @Schema(description = "회원 고유 pk")
-    @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
-    private User user;
+    private Long userId; // User 엔티티와 관계가 있지만, DTO에서는 userId만 전달
 
     @Schema(description = "청약통장종류")
-    @Column(name = "type", nullable = true)
-    @Enumerated(EnumType.STRING)
     private AccountType type;
 
     @NotNull
     @Schema(description = "청약통장가입일")
-    @Column(name = "created_at", nullable = false)
     private LocalDate createdAt;
 
     @Schema(description = "납입횟수")
-    @Column(name = "payment_count", nullable = true)
     private Integer paymentCount;
 
     @Schema(description = "총 납입금액 (단위:만원)")
-    @Column(name = "total_amount", nullable = true)
     private Integer totalAmount;
 
     @Schema(description = "납입인정금액 (단위:만원)")
-    @Column(name = "recognized_amount", nullable = true)
     private Integer recognizedAmount;
 
     @NotNull
     @Schema(description = "통장명의 (1: 본인, 2: 배우자)")
-    @Column(name = "relationship", nullable = false)
     private Integer relationship;
 }
