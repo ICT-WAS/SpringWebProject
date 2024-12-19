b9fee86dc660b4ae340deb289a6081e714e35938
mmparksb <mmparksb@gmail.com>
2024-12-19 18:09:34 +0900

    [스프링] 관심 공고 일정별, 커뮤니티 관련 알림 구현 완료
    - 관심 공고의 접수 시작일, 종료일, 당첨자 발표일에 알림을 보내는 기능 구현
    - 커뮤니티 댓글, 답글 작성시 연관된 유저들에게 알림 보내는 기능 구현

--- home/src/main/java/com/ict/home/HomeApplication.java
+++ home/src/main/java/com/ict/home/HomeApplication.java
@@ -2,8 +2,10 @@ package com.ict.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HomeApplication {
 
 	public static void main(String[] args) {
