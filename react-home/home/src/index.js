e2038052f88a2884b2e03376571595b43d5c8275
Lee-Hyeonji99 <padul1210@gmail.com>
2024-12-22 00:35:04 +0900

    [리액트, 스프링] 네이버지도 API 지오코드 <=> 지도 시각화 리액트에서 스프링으로 위치 정보를 넘긴 뒤 네이버지도 API 지오코드 결과의 위도, 경도를 다시 스프링으로 응답받은 결과를 시각화

--- react-home/home/src/index.js
+++ react-home/home/src/index.js
@@ -38,160 +38,165 @@ import InterestList from "./mypage/InterestList";
 import MyPageContent from "./mypage/MyPageContent";
 import CollectionCommentPost from "./mypage/CollectionCommentPost";
 import CollectionPost from "./mypage/CollectionPost";
+import { NavermapsProvider } from "react-naver-maps";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AppProvider>
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <React.StrictMode>
-              <Main />
-            </React.StrictMode>
-          }
-        />
+  <NavermapsProvider
+    ncpClientId='pvkq1et62k'
+  >
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <React.StrictMode>
+                <Main />
+              </React.StrictMode>
+            }
+          />
 
-        {/* 청약 */}
-        <Route path="/subscriptions" element={<ApplyAnnouncementList />} />
-        <Route
-          path="/subscriptions/info/:houseId"
-          element={<ApplyAnnouncement />}
-        />
-        <Route
-          path="/subscriptions/search/:keyword"
-          element={<SearchAnnouncementByName />}
-        />
+          {/* 청약 */}
+          <Route path="/subscriptions" element={<ApplyAnnouncementList />} />
+          <Route
+            path="/subscriptions/info/:houseId"
+            element={<ApplyAnnouncement />}
+          />
+          <Route
+            path="/subscriptions/search/:keyword"
+            element={<SearchAnnouncementByName />}
+          />
 
-        {/* 조건등록 */}
-        <Route path="/conditions" element={<Conditions />} />
-        <Route path="/condition-1" element={<Condition01 />} />
-        <Route path="/condition-2" element={<Condition02 />} />
-        <Route path="/condition-3" element={<Condition03 />} />
+          {/* 조건등록 */}
+          <Route path="/conditions" element={<Conditions />} />
+          <Route path="/condition-1" element={<Condition01 />} />
+          <Route path="/condition-2" element={<Condition02 />} />
+          <Route path="/condition-3" element={<Condition03 />} />
 
-        {/* 마이페이지 */}
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/interest" element={<InterestList />} />
-        <Route path="/myposts" element={<CollectionPost />} />
-        <Route path="/mycomments" element={<CollectionCommentPost />} />
+          {/* 마이페이지 */}
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/interest" element={<InterestList />} />
+          <Route path="/myposts" element={<CollectionPost />} />
+          <Route path="/mycomments" element={<CollectionCommentPost />} />
 
-        {/* 커뮤니티 */}
-        <Route exact path="/community" element={<CommunityPostList />} />
-        <Route exact path="/community/posting" element={<CommunityPosting />} />
-        <Route exact path="/community/:postId" element={<PostDetail />} />
-        <Route
-          exact
-          path="/community/posting/:postId"
-          element={<UpdatePost />}
-        />
+          {/* 커뮤니티 */}
+          <Route exact path="/community" element={<CommunityPostList />} />
+          <Route exact path="/community/posting" element={<CommunityPosting />} />
+          <Route exact path="/community/:postId" element={<PostDetail />} />
+          <Route
+            exact
+            path="/community/posting/:postId"
+            element={<UpdatePost />}
+          />
 
-        {/* 소셜 로그인 리다이렉트 경로 */}
-        <Route exact path="/kakao/callback" element={<KakaoRedirection />} />
-        <Route exact path="/naver/callback" element={<NaverRedirection />} />
+          {/* 소셜 로그인 리다이렉트 경로 */}
+          <Route exact path="/kakao/callback" element={<KakaoRedirection />} />
+          <Route exact path="/naver/callback" element={<NaverRedirection />} />
 
-        {/* 로그인한 사용자는 접근할 수 없음 - LoginRoute 컴포넌트로 감싸면 보호 설정 됨 */}
-        {/* 로그인한 사용자가 페이지 접근 시 메인 페이지("/")로 리다이렉트 */}
-        {/* 회원가입 */}
-        <Route
-          path="/signup"
-          element={
-            <LoginRoute>
-              <Signup />
-            </LoginRoute>
-          }
-        />
+          {/* 로그인한 사용자는 접근할 수 없음 - LoginRoute 컴포넌트로 감싸면 보호 설정 됨 */}
+          {/* 로그인한 사용자가 페이지 접근 시 메인 페이지("/")로 리다이렉트 */}
+          {/* 회원가입 */}
+          <Route
+            path="/signup"
+            element={
+              <LoginRoute>
+                <Signup />
+              </LoginRoute>
+            }
+          />
 
-        {/* 로그인 */}
-        <Route
-          path="/login"
-          element={
-            <LoginRoute>
-              <Login />
-            </LoginRoute>
-          }
-        />
+          {/* 로그인 */}
+          <Route
+            path="/login"
+            element={
+              <LoginRoute>
+                <Login />
+              </LoginRoute>
+            }
+          />
 
-        {/* 이메일 찾기 */}
-        <Route
-          path="/find-email"
-          element={
-            <LoginRoute>
-              <FindEmail />
-            </LoginRoute>
-          }
-        />
-        <Route
-          exact
-          path="/find-email/verify"
-          element={
-            <LoginRoute>
-              <EmailRecoveryResult />
-            </LoginRoute>
-          }
-        />
+          {/* 이메일 찾기 */}
+          <Route
+            path="/find-email"
+            element={
+              <LoginRoute>
+                <FindEmail />
+              </LoginRoute>
+            }
+          />
+          <Route
+            exact
+            path="/find-email/verify"
+            element={
+              <LoginRoute>
+                <EmailRecoveryResult />
+              </LoginRoute>
+            }
+          />
 
-        {/* 비밀번호 찾기 */}
-        <Route
-          exact
-          path="/find-password"
-          element={
-            <LoginRoute>
-              <FindPassword />
-            </LoginRoute>
-          }
-        />
-        <Route
-          exact
-          path="/find-password/verify"
-          element={
-            <LoginRoute>
-              <SendVerification />
-            </LoginRoute>
-          }
-        />
-        <Route
-          exact
-          path="/find-password/reset"
-          element={
-            <LoginRoute>
-              <ResetPassword />
-            </LoginRoute>
-          }
-        />
+          {/* 비밀번호 찾기 */}
+          <Route
+            exact
+            path="/find-password"
+            element={
+              <LoginRoute>
+                <FindPassword />
+              </LoginRoute>
+            }
+          />
+          <Route
+            exact
+            path="/find-password/verify"
+            element={
+              <LoginRoute>
+                <SendVerification />
+              </LoginRoute>
+            }
+          />
+          <Route
+            exact
+            path="/find-password/reset"
+            element={
+              <LoginRoute>
+                <ResetPassword />
+              </LoginRoute>
+            }
+          />
 
-        {/* 로그인한 사용자만 접근할 수 있는 페이지 - PrivateRoute 컴포넌트로 감싸면 보호 설정 됨 */}
-        {/* 로그인하지 않으면 로그인 페이지(/login)로 리다이렉트 */}
-        {/* 회원 정보 조회 */}
-        <Route
-          path="/mypage/account"
-          element={
-            <PrivateRoute>
-              <UserInfo />
-            </PrivateRoute>
-          }
-        />
-        {/* 회원 정보 수정 */}
-        <Route
-          path="/mypage/account/edit"
-          element={
-            <PrivateRoute>
-              <UserInfoEdit />
-            </PrivateRoute>
-          }
-        />
-        {/* 테스트 페이지입니다. -hw */}
-        <Route
-          path="/test"
-          element={
-            <PrivateRoute>
-              <UserTest />
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-    </Router>
-  </AppProvider>
+          {/* 로그인한 사용자만 접근할 수 있는 페이지 - PrivateRoute 컴포넌트로 감싸면 보호 설정 됨 */}
+          {/* 로그인하지 않으면 로그인 페이지(/login)로 리다이렉트 */}
+          {/* 회원 정보 조회 */}
+          <Route
+            path="/mypage/account"
+            element={
+              <PrivateRoute>
+                <UserInfo />
+              </PrivateRoute>
+            }
+          />
+          {/* 회원 정보 수정 */}
+          <Route
+            path="/mypage/account/edit"
+            element={
+              <PrivateRoute>
+                <UserInfoEdit />
+              </PrivateRoute>
+            }
+          />
+          {/* 테스트 페이지입니다. -hw */}
+          <Route
+            path="/test"
+            element={
+              <PrivateRoute>
+                <UserTest />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </AppProvider>
+  </NavermapsProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
